86c4af7ef6740a250f9b1a473d6b26427451e4d5
Megan Yu <meganyu@fb.com>
2020-07-06 08:42:30 -0700

    Initial Commit

---
+++ snappergram/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  snappergram
+//
+//  Created by meganyu on 7/6/20.
+//  Copyright © 2020 meganyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ snappergram/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  snappergram
+//
+//  Created by meganyu on 7/6/20.
+//  Copyright © 2020 meganyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
