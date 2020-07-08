33ab4016c9e42720ab291e72ca6b477b5400fa6f
Megan Yu <meganyu@fb.com>
2020-07-08 15:31:02 -0700

    Added infinite scrolling to load more posts once user reaches bottom of screen

--- snappergram/View Controllers/Home View Controller/HomeViewController.m
+++ snappergram/View Controllers/Home View Controller/HomeViewController.m
@@ -7,12 +7,14 @@
 //
 
 #import "HomeViewController.h"
-#import <Parse/Parse.h>
-#import "SceneDelegate.h"
+
+#import "InfiniteScrollActivityView.h"
 #import "LoginViewController.h"
+#import <Parse/Parse.h>
 #import "Post.h"
 #import "PostCollectionCell.h"
 #import "PostDetailsViewController.h"
+#import "SceneDelegate.h"
 
 static NSString *const loginViewControllerID = @"LoginViewController";
 static NSString *const mainStoryboardID = @"Main";
@@ -20,18 +22,22 @@ static NSString *const kPostDetailsSegueID = @"postDetailsSegue";
 
 #pragma mark - Interface
 
-@interface HomeViewController () <PostCollectionCellDelegate, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout>
+@interface HomeViewController () <PostCollectionCellDelegate, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UIScrollViewDelegate>
 
 @property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
 @property (weak, nonatomic) UICollectionViewFlowLayout *flowLayout;
 @property (strong, nonatomic) NSArray *postArray;
 @property (nonatomic, strong) UIRefreshControl *refreshControl;
+@property int postQueryLimit;
 
 @end
 
 #pragma mark - Implementation
 
 @implementation HomeViewController
+
+bool isMoreDataLoading = false;
+InfiniteScrollActivityView* loadingMoreView;
 
 #pragma mark - Setup
 
@@ -45,23 +51,38 @@ static NSString *const kPostDetailsSegueID = @"postDetailsSegue";
     self.flowLayout.minimumInteritemSpacing = 0;
     self.flowLayout.minimumLineSpacing = 0;
     
+    self.postQueryLimit = 20;
     [self loadPosts];
     
     self.refreshControl = [[UIRefreshControl alloc] init];
     [self.refreshControl addTarget:self action:@selector(loadPosts) forControlEvents:UIControlEventValueChanged];
     [self.collectionView insertSubview:self.refreshControl atIndex:0];
+    
+    // Set up Infinite Scroll loading indicator
+    CGRect frame = CGRectMake(0, self.collectionView.contentSize.height, self.collectionView.bounds.size.width, InfiniteScrollActivityView.defaultHeight);
+    loadingMoreView = [[InfiniteScrollActivityView alloc] initWithFrame:frame];
+    loadingMoreView.hidden = true;
+    [self.collectionView addSubview:loadingMoreView];
+    
+    UIEdgeInsets insets = self.collectionView.contentInset;
+    insets.bottom += InfiniteScrollActivityView.defaultHeight;
+    self.collectionView.contentInset = insets;
 }
 
 - (void)loadPosts {
-    // construct PFQuery
     PFQuery *postQuery = [Post query];
     [postQuery orderByDescending:@"createdAt"];
     [postQuery includeKey:@"author"];
-    postQuery.limit = 20;
+    postQuery.limit = _postQueryLimit;
+    postQuery.cachePolicy = kPFCachePolicyNetworkElseCache;
 
     // fetch data asynchronously
     [postQuery findObjectsInBackgroundWithBlock:^(NSArray<Post *> * _Nullable posts, NSError * _Nullable error) {
         if (posts) {
+            isMoreDataLoading = false;
+            [loadingMoreView stopAnimating];
+            self.postQueryLimit = (int) posts.count + 5;
+            
             self.postArray = posts;
             [self.collectionView reloadData];
         }
@@ -116,6 +137,29 @@ static NSString *const kPostDetailsSegueID = @"postDetailsSegue";
     [self performSegueWithIdentifier:kPostDetailsSegueID sender:post];
 }
 
+#pragma mark - UIScrollViewDelegate methods
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+    if(!isMoreDataLoading){
+       // Calculate the position of one screen length before the bottom of the results
+       int scrollViewContentHeight = self.collectionView.contentSize.height;
+       int scrollOffsetThreshold = scrollViewContentHeight - self.collectionView.bounds.size.height;
+       
+       // When the user has scrolled past the threshold, start requesting
+       if(scrollView.contentOffset.y > scrollOffsetThreshold && self.collectionView.isDragging) {
+           isMoreDataLoading = true;
+           
+           // Update position of loadingMoreView, and start loading indicator
+           CGRect frame = CGRectMake(0, self.collectionView.contentSize.height, self.collectionView.bounds.size.width, InfiniteScrollActivityView.defaultHeight);
+           loadingMoreView.frame = frame;
+           [loadingMoreView startAnimating];
+           
+           // ... Code to load more results ...
+           NSLog(@"Letting you know that we're pulling up more data! Post query limit: %d", _postQueryLimit);
+           [self loadPosts];
+       }
+    }
+}
 
 #pragma mark - Navigation
 
